1943112a8378dfd72488d8c2c50eba5fce0ea009
harshitp <palacheralaharshit@gmail.com>
2024-11-25 00:39:51 +0530

    Load more functionality

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 //import Ratings from './components/Ratings';
-import ImageSlider from './components/imageSlider';
+// import ImageSlider from './components/imageSlider';
+import LoadMoreData from './components/load more data';
 function App() {
   return (
     <div className="App">
       {/*<Ratings noOfStars={10} /> */}
-      <ImageSlider url="https://picsum.photos/v2/list" page={"1"} limit={"10"} />
+      {/* <ImageSlider url="https://picsum.photos/v2/list" page={"1"} limit={"10"} /> */}
+      <LoadMoreData/>
+
     </div>
   );
 }
